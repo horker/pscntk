5836a758a585696e43552b4140ee1552c5510add
horker <horker@users.noreply.github.com>
2018-08-15 02:41:06 +0900

    Implement cntk.conv

--- source/Horker.PSCNTK/Classes/Layers.cs
+++ source/Horker.PSCNTK/Classes/Layers.cs
@@ -7,12 +7,12 @@ namespace Horker.PSCNTK
 {
     public class Layers
     {
-        public static Function Dense(Variable input, Shape outputShape, CNTKDictionary initializer = null, CNTKDictionary biasInitializer = null, bool hasBias = true, string activation = null)
+        public static Function Dense(Variable input, Shape outputShape, CNTKDictionary initializer, bool useBias, CNTKDictionary biasInitializer, string activation, string name)
         {
             if (initializer == null)
                 initializer = CNTKLib.GlorotUniformInitializer();
 
-            if (biasInitializer == null)
+            if (useBias && biasInitializer == null)
                 biasInitializer = CNTKLib.ConstantInitializer(0);
 
             if (input.Shape.Rank > 1)
@@ -29,7 +29,7 @@ namespace Horker.PSCNTK
 
             Function output;
 
-            if (hasBias)
+            if (useBias)
             {
                 var bias = new Parameter(new int[] { hiddenSize }, DataType.Float, biasInitializer);
                 output = CNTKLib.Plus(CNTKLib.Times(weight, input), bias);
@@ -40,13 +40,71 @@ namespace Horker.PSCNTK
             if (outputShape.Rank > 1)
                 output = CNTKLib.Reshape(output, outputShape.Dimensions);
 
-            if (activation != null)
-            {
-                var m = Helpers.GetCNTKLibMethod(activation);
-                output = (Function)m.Invoke(null, new object[] { output });
-            }
+            output = Helpers.ApplyActivation(output, activation);
 
             return output;
+        }
+
+        public static Function Convolution(Variable input, int[] filterShape, int numFilters, string activation, CNTKDictionary initializer, bool useBias, CNTKDictionary biasInitializer, int[] strides, bool[] sharing, bool[] padding, int[] dilation, int reductionRank, int groups, int maxTempMemSizeInSamples, string name)
+        {
+            // Assume input variable's dimensions = (x, [y, [z,]] channels)
+
+            if (filterShape.Length != input.Shape.Rank - 1)
+                throw new ArgumentException("Length of filterShape should match input's dimension minus one");
+
+            if (strides.Length != 1 && strides.Length != input.Shape.Rank - 1)
+                throw new ArgumentException("Length of strides should be one, or match input's dimension minus one");
+
+            // Initializers
+
+            if (initializer == null)
+                initializer = CNTKLib.GlorotUniformInitializer();
+
+            if (useBias && biasInitializer == null)
+                biasInitializer = CNTKLib.ConstantInitializer(0);
+
+            // Convolution map
+
+            var convDims = new int[filterShape.Length + 2];
+            filterShape.CopyTo(convDims, 0);
+            convDims[filterShape.Length] = input.Shape.Dimensions[filterShape.Length]; // number of channels
+            convDims[filterShape.Length + 1] = numFilters; // feature map count
+
+            var convolutionMap = new Parameter(convDims, DataType.Float, initializer);
+
+            // Strides
+
+            var st = new int[input.Shape.Rank]; // plus channel
+            if (strides.Length == 1)
+                for (var i = 0; i < input.Shape.Rank - 1; ++i)
+                    st[i] = strides[0];
+            else
+                strides.CopyTo(st, 0);
+
+            st[input.Shape.Rank - 1] = numFilters;
+
+            var conv = CNTKLib.Convolution(
+                convolutionMap,                // CNTK.Variable convolutionMap
+                input,                         // CNTK.Variable operand
+                st,                            // CNTK.NDShape strides
+                new BoolVector(sharing),       // CNTK.BoolVector sharing
+                new BoolVector(padding),       // CNTK.BoolVector autoPadding
+                dilation,                      // CNTK.NDShape dilation
+                (uint)reductionRank,           // uint reductionRank
+                (uint)groups,                  // uint groups
+                (uint)maxTempMemSizeInSamples, // uint maxTempMemSizeInSamples
+                name                           // string name
+            );
+
+            if (useBias)
+            {
+                var bias = new Parameter(conv.Output.Shape, DataType.Float, biasInitializer);
+                conv = CNTKLib.Plus(conv, bias);
+            }
+
+            conv = Helpers.ApplyActivation(conv, activation);
+
+            return conv;
         }
 
         public static Function OptimizedRNNStack(Variable input, int hiddenSize, int layerSize = 1, bool bidirectional = false, string cellType = "lstm", string name = "")
--- source/Horker.PSCNTK/Cmdlets/LayerCmdlets.cs
+++ source/Horker.PSCNTK/Cmdlets/LayerCmdlets.cs
@@ -51,20 +51,92 @@ namespace Horker.PSCNTK
         public string Activation = null;
 
         [Parameter(Position = 4, Mandatory = false)]
-        public CNTKDictionary BiasInitializer = null;
+        public SwitchParameter NoBias = false;
 
         [Parameter(Position = 5, Mandatory = false)]
-        public SwitchParameter NoBias = false;
+        public CNTKDictionary BiasInitializer = null;
+
+        [Parameter(Position = 6, Mandatory = false)]
+        public string Name = "";
 
         protected override void EndProcessing()
         {
-            if (Initializer == null)
-                Initializer = CNTKLib.GlorotUniformInitializer();
+            var output = Layers.Dense(
+                Input,           // Variable input,
+                Shape,           // Shape outputShape,
+                Initializer,     // CNTKDictionary initializer,
+                !NoBias,         // bool hasBias,
+                BiasInitializer, // CNTKDictionary biasInitializer,
+                Activation,      // string activation
+                Name             // string name
+            );
 
-            if (BiasInitializer == null)
-                BiasInitializer = CNTKLib.ConstantInitializer(0);
+            WriteObject(output);
+        }
+    }
 
-            var output = Layers.Dense(Input, Shape, Initializer, BiasInitializer, !NoBias, Activation);
+    [Cmdlet("New", "CNTKConv")]
+    [Alias("cntk.conv")]
+    public class NewCNTKConv : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public CNTK.Variable Input;
+
+        [Parameter(Position = 1, Mandatory = true)]
+        public int[] FilterShape;
+
+        [Parameter(Position = 2, Mandatory = true)]
+        public int NumFilters;
+
+        [Parameter(Position = 3, Mandatory = false)]
+        public CNTKDictionary Initializer = null;
+
+        [Parameter(Position = 4, Mandatory = false)]
+        public string Activation = null;
+
+        [Parameter(Position = 5, Mandatory = false)]
+        public bool[] Pad = new bool[] { true };
+
+        [Parameter(Position = 6, Mandatory = false)]
+        public int[] Strides = new int[] { 1 };
+
+        [Parameter(Position = 7, Mandatory = false)]
+        public bool[] Sharing = new bool[] { true };
+
+        [Parameter(Position = 8, Mandatory = false)]
+        public SwitchParameter NoBias = false;
+
+        [Parameter(Position = 9, Mandatory = false)]
+        public CNTKDictionary biasInitializer = null;
+
+        [Parameter(Position = 10, Mandatory = false)]
+        public int ReductionRank = 1;
+
+        [Parameter(Position = 11, Mandatory = false)]
+        public int MaxTempMemSizeInSamples = 0;
+
+        [Parameter(Position = 12, Mandatory = false)]
+        public string Name = "";
+
+        protected override void EndProcessing()
+        {
+            var output = Layers.Convolution(
+                Input,                   // Variable input
+                FilterShape,             // int[] filterShape
+                NumFilters,              // int numFilters
+                Activation,              // string activation
+                Initializer,             // CNTKDictionary initializer
+                !NoBias,                 // bool hasBias
+                biasInitializer,         // CNTKDictionary biasInitializer
+                Strides,                 // int[] strides
+                Sharing,                 // bool[] sharing
+                Pad,                     // bool[] padding
+                new int[] { 1 },         // int[] dilation
+                ReductionRank,           // int reductionRank
+                1,                       // int groups
+                MaxTempMemSizeInSamples, // int maxTempMemSizeInSamples
+                Name                     // string name
+            );
 
             WriteObject(output);
         }
