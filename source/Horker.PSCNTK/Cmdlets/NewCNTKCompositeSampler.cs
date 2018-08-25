ae217b6ed44a4985c6671515a518ad2595900df6
horker <horker@users.noreply.github.com>
2018-08-26 06:49:31 +0900

    Implement CompositeSampler

---
+++ source/Horker.PSCNTK/Cmdlets/NewCNTKCompositeSampler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+using CNTK;
+
+namespace Horker.PSCNTK
+{
+    [Cmdlet("New", "CNTKCompositeSampler")]
+    [Alias("cntk.compositesampler")]
+    public class NewCNTKCompositeSampler : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public ISampler[] Samplers;
+
+        protected override void EndProcessing()
+        {
+            var sampler = new CompositeSampler(Samplers);
+            WriteObject(sampler);
+        }
+    }
+}
---
+++ source/Horker.PSCNTK/Samplers/CompositeSampler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CNTK;
+
+namespace Horker.PSCNTK
+{
+    [Serializable]
+    public class CompositeSampler : ISampler
+    {
+        private ISampler[] _samplers;
+
+        public IEnumerable<ISampler> Samplers;
+
+        public CompositeSampler(params ISampler[] samplers)
+        {
+            _samplers = samplers;
+        }
+
+        public Minibatch GetNextBatch(DeviceDescriptor device = null)
+        {
+            return new Minibatch(_samplers.Select(x => x.GetNextBatch(device)));
+        }
+
+        public Minibatch GetValidationBatch(DeviceDescriptor device = null)
+        {
+            return new Minibatch(_samplers.Select(x => x.GetValidationBatch(device)));
+        }
+    }
+}
