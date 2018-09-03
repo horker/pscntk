573f3c3be71d100e0efadce1e7f0e49def2d4a0f
horker <horker@users.noreply.github.com>
2018-09-04 02:56:31 +0900

    Refactor New-CNTKTrainer

---
+++ source/Horker.PSCNTK/Cmdlets/CmdletHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Management.Automation;
+using CNTK;
+
+namespace Horker.PSCNTK
+{
+    class CmdletHelpers
+    {
+        public static Function GetFunctionInstance(object func, object[] arguments, string displayName)
+        {
+            if (func == null)
+                return null;
+
+            if (func is PSObject)
+                func = (func as PSObject).BaseObject;
+
+            if (func is Function)
+            {
+                return func as Function;
+            }
+            else if (func is string)
+            {
+                var f = func as string;
+                if (string.IsNullOrEmpty(f))
+                    return null;
+
+                var lossMethod = Helpers.GetCNTKLibMethod(f, arguments.Length);
+                return (Function)lossMethod.Invoke(null, arguments);
+            }
+
+            throw new ArgumentException(displayName + " should be an instance of Function or a function name");
+        }
+    }
+}
--- source/Horker.PSCNTK/Cmdlets/NewCNTKTrainer.cs
+++ source/Horker.PSCNTK/Cmdlets/NewCNTKTrainer.cs
@@ -27,35 +27,10 @@ namespace Horker.PSCNTK
         [Parameter(Position = 4, Mandatory = true)]
         public Learner[] Learners;
 
-        private Function GetFunctionInstance(object func, string displayName)
-        {
-            if (func == null)
-                return null;
-
-            if (func is PSObject)
-                func = (func as PSObject).BaseObject;
-
-            if (func is Function)
-            {
-                return func as Function;
-            }
-            else if (func is string)
-            {
-                var f = func as string;
-                if (string.IsNullOrEmpty(f))
-                    return null;
-
-                var lossMethod = Helpers.GetCNTKLibMethod(f, 2);
-                return (Function)lossMethod.Invoke(null, new object[] { Model, Label });
-            }
-
-            throw new ArgumentException(displayName + " should be an instance of Function or a function name");
-        }
-
         protected override void EndProcessing()
         {
-            Function loss = GetFunctionInstance(LossFunction, "LossFunction");
-            Function error = GetFunctionInstance(ErrorFunction, "ErrorFunction");
+            Function loss = CmdletHelpers.GetFunctionInstance(LossFunction, new object[] { Model, Label }, "LossFunction");
+            Function error = CmdletHelpers.GetFunctionInstance(ErrorFunction, new object[] { Model, Label }, "ErrorFunction");
 
             var trainer = Trainer.CreateTrainer(Model, loss, error, Learners.ToList());
             WriteObject(trainer);
