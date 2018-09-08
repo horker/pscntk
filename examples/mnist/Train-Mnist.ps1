74d9ecca68402df9eafc4b602a212a9dba3763c8
horker <horker@users.noreply.github.com>
2018-09-08 21:46:49 +0900

    Divide examples/mnist/Train-Mnist.ps1 to two files

--- examples/mnist/Train-Mnist.ps1
+++ examples/mnist/Train-Mnist.ps1
@@ -5,80 +5,46 @@ param(
 Set-StrictMode -Version Latest
 
 ############################################################
-# MNIST data files
+# Configuration
 ############################################################
-
-# Before running this program, do the following:
-#
-# (1) Download the MNIST data files from the following web site:
-#       url: http://yann.lecun.com/exdb/mnist/
-#       files: train-images-idx3-ubyte.gz train-labels-idx1-ubytes.gz
-#
-# (2) Unzip them and place the files in <project dir>\data\MNIST
-
-$MNIST_IMAGE_FILE = "$PSScriptRoot\..\..\data\MNIST\train-images.idx3-ubyte"
-$MNIST_LABEL_FILE = "$PSScriptRoot\..\..\data\MNIST\train-labels.idx1-ubyte"
 
 $MNIST_CACHE_FILE = "$PSScriptRoot\mnist_data.bin"
 
+$OUT_CLASSES = 10
+
+Set-CNTKRandomSeed 1234
+
 ############################################################
-# Prepraring data
+# Data
 ############################################################
 
-if (Test-Path $MNIST_CACHE_FILE) {
-  $sampler = cntk.sampler -Path $MNIST_CACHE_FILE
-}
-else {
-  Write-Host "Loading MNIST image file..."
-  $data = [System.IO.File]::ReadAllBytes($MNIST_IMAGE_FILE)
-
-  Write-Host "Preprocessing images..."
-  $data = $data.Slice(@(16, 0)) # Skip header
-  $data = $data.Scale(0, 255, 0, 1.0)
-  $data = cntk.datasource $data (28, 28, 1, 1, -1)
-
-  Write-Host "Loading MNIST label file..."
-  $label = [System.IO.File]::ReadAllBytes($MNIST_LABEL_FILE)
-
-  Write-Host "Preprocessing labels..."
-  $label = $label.Slice(@(8, 0))
-  $label = $label.OneHot(10).ToFlatArray($true)
-  $label = cntk.datasource $label (10, 1, -1)
-
-  Write-Host "Saving..."
-  $sampler = cntk.sampler @{ input = $data; label = $label }
-  $sampler.Save($MNIST_CACHE_FILE)
-}
-
-$sampler.MinibatchSize = 64
-$sampler.ValidationRate = .2
+$data = cntk.datasourceset -Path $MNIST_CACHE_FILE
+$sampler = cntk.sampler @{ input = $data["input"]; label = $data["label"] } -MinibatchSize 64 -ValidationRate .2
 
 ############################################################
 # Model
 ############################################################
-
-$OUT_CLASSES = 10
 
 $in = cntk.input (28, 28, 1) -Name input
 $n = $in
 
 if ($UseConv) {
   # conv1: 28 x 28 x 1 -> 28 x 28 x 8 -> 14 x 14 x 8
-  $n = cntk.conv2d $n (3, 3) 8 (1, 1) $true (cntk.truncatednormal .1) relu
+  $n = cntk.conv2d $n (3, 3) 8 (1, 1) $true (cntk.init.truncatednormal .1) relu
   $n = cntk.maxpooling $n (3, 3) (2, 2)
 
   # conv2: 14 x 14 x 8 -> 14 x 14 x 32 -> 7 x 7 x 32
-  $n = cntk.conv2d $n (3, 3) 32 (1, 1) $true (cntk.truncatednormal .1) relu
+  $n = cntk.conv2d $n (3, 3) 32 (1, 1) $true (cntk.init.truncatednormal .1) relu
   $n = cntk.maxpooling $n (3, 3) (2, 2)
 
   # fc
-  $n = cntk.dense $n $OUT_CLASSES (cntk.glorotuniform)
+  $n = cntk.dense $n $OUT_CLASSES (cntk.init.glorotuniform)
 }
 else {
-  $n = cntk.dense $n 1000 (cntk.heuniform) relu
-  $n = cntk.dense $n 1000 (cntk.heuniform) relu
-  $n = cntk.dense $n 1000 (cntk.heuniform) relu
-  $n = cntk.dense $n $OUT_CLASSES (cntk.glorotuniform)
+  $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
+  $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
+  $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
+  $n = cntk.dense $n $OUT_CLASSES (cntk.init.glorotuniform)
 }
 
 $out = $n
