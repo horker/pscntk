4cb5a5a33368e2a9ddfd9cdb2c4232b249964155
horker <horker@users.noreply.github.com>
2018-09-11 00:10:04 +0900

    Make cntk.load return a WrappedFunction object

---
+++ source/Horker.PSCNTK/Cmdlets/LoadCNTKFunction.cs
@@ -0,0 +1,25 @@
+﻿using System.Management.Automation;
+using CNTK;
+
+namespace Horker.PSCNTK
+{
+    [Cmdlet("Restore", "CNTKFunction")]
+    [Alias("cntk.restore", "cntk.load")]
+    public class LoadCNTKFunction : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string Path;
+
+        [Parameter(Position = 1, Mandatory = false)]
+        public DeviceDescriptor Device = DeviceDescriptor.UseDefaultDevice();
+
+        [Parameter(Position = 2, Mandatory = false)]
+        public ModelFormat Format = ModelFormat.CNTKv2;
+
+        protected override void EndProcessing()
+        {
+            var result = Function.Load(Path, Device, Format);
+            WriteObject(new WrappedFunction(result));
+        }
+    }
+}
--- source/Horker.PSCNTK/Cmdlets/VariableCmdlets.cs
+++ source/Horker.PSCNTK/Cmdlets/VariableCmdlets.cs
@@ -109,24 +109,4 @@ namespace Horker.PSCNTK
             WriteObject(new WrappedVariable(result));
         }
     }
-
-    [Cmdlet("Restore", "CNTKFunction")]
-    [Alias("cntk.restore", "cntk.load")]
-    public class LoadCNTKFunction : PSCmdlet
-    {
-        [Parameter(Position = 0, Mandatory = true)]
-        public string Path;
-
-        [Parameter(Position = 1, Mandatory = false)]
-        public DeviceDescriptor Device = DeviceDescriptor.UseDefaultDevice();
-
-        [Parameter(Position = 2, Mandatory = false)]
-        public ModelFormat Format = ModelFormat.CNTKv2;
-
-        protected override void EndProcessing()
-        {
-            var result = Function.Load(Path, Device, Format);
-            WriteObject(result);
-        }
-    }
 }
