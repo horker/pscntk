adc3339fbe940ab07bb8ee5cc2a3dcd767e11010
horker <horker@users.noreply.github.com>
2018-09-12 03:07:52 +0900

    Implement device cmdlets

---
+++ source/Horker.PSCNTK/Cmdlets/DeviceCmdlets.cs
@@ -0,0 +1,86 @@
+﻿using System.Management.Automation;
+using System.Runtime.InteropServices;
+using CNTK;
+
+namespace Horker.PSCNTK
+{
+    [Cmdlet("New", "CNTKDevice")]
+    [Alias("cntk.device")]
+    public class GetCNTKDevice : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "gpu")]
+        public int GPUDeviceId;
+
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "cpu")]
+        public SwitchParameter CPUDevice;
+
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "default")]
+        public SwitchParameter DefaultDevice;
+
+        protected override void EndProcessing()
+        {
+            DeviceDescriptor device;
+
+            if (CPUDevice)
+                device = DeviceDescriptor.CPUDevice;
+            else if (DefaultDevice)
+                device = DeviceDescriptor.UseDefaultDevice();
+            else
+                device = DeviceDescriptor.GPUDevice(GPUDeviceId);
+
+            WriteObject(device);
+        }
+    }
+
+    [Cmdlet("Get", "CNTKAllDevices")]
+    [Alias("cntk.alldevices")]
+    public class GetCNTKAllDevices : PSCmdlet
+    {
+        protected override void EndProcessing()
+        {
+            foreach (var device in DeviceDescriptor.AllDevices())
+                WriteObject(device);
+        }
+    }
+
+    [Cmdlet("Set", "CNTKDefaultDevice")]
+    [Alias("cntk.setdefaultdevice")]
+    public class SetCNTKDefaultDevice : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "gpu")]
+        public int GPUDeviceId;
+
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "cpu")]
+        public SwitchParameter CPUDevice;
+
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "default")]
+        public SwitchParameter DefaultDevice;
+
+        protected override void EndProcessing()
+        {
+            DeviceDescriptor device;
+
+            if (CPUDevice)
+                device = DeviceDescriptor.CPUDevice;
+            else if (DefaultDevice)
+                device = DeviceDescriptor.UseDefaultDevice();
+            else
+                device = DeviceDescriptor.GPUDevice(GPUDeviceId);
+
+            WriteObject(DeviceDescriptor.TrySetDefaultDevice(device));
+        }
+    }
+
+    [Cmdlet("Set", "CNTKExcludedDevice")]
+    [Alias("cntk.setexcludeddevice")]
+    public class SetCNTKExcludedDevice : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public DeviceDescriptor[] Devices;
+
+        protected override void EndProcessing()
+        {
+            DeviceDescriptor.SetExcludedDevices(Devices);
+        }
+    }
+}
