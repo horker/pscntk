bb3ae8f95ec518268b1ae5959b270a311b165e3f
horker <horker@users.noreply.github.com>
2018-09-21 11:10:56 +0900

    Implement cntk.batchofsequenses (experimental)

---
+++ source/Horker.PSCNTK/Cmdlets/ValueCmdlets.cs
@@ -0,0 +1,28 @@
+﻿using System.Management.Automation;
+using CNTK;
+
+namespace Horker.PSCNTK
+{
+    [Cmdlet("New", "CNTKBatchOfSequences")]
+    [Alias("cntk.batchofsequences")]
+    public class NewCNTKBatchOfSequenses : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public int[] Dimensions;
+
+        [Parameter(Position = 1, Mandatory = true)]
+        public float[][] Values;
+
+        [Parameter(Position = 2, Mandatory = false)]
+        public DeviceDescriptor Device = null;
+
+        protected override void EndProcessing()
+        {
+            if (Device == null)
+                Device = DeviceDescriptor.UseDefaultDevice();
+
+            var values = Value.CreateBatchOfSequences(Dimensions, Values, Device, false);
+            WriteObject(values);
+        }
+    }
+}
