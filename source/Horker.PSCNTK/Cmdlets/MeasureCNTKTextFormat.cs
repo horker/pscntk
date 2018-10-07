c2ebdb7256d661abb82186a4975d9487db273a5c
horker <horker@users.noreply.github.com>
2018-10-08 02:34:14 +0900

    Implement Measure-CNTKTextFormat

---
+++ source/Horker.PSCNTK/Cmdlets/MeasureCNTKTextFormat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Horker.PSCNTK
+{
+    public class CTFLineInfo
+    {
+        public int Lines;
+        public int Sequences;
+    }
+
+    [Cmdlet("Measure", "CNTKTextFormat")]
+    [OutputType(typeof(CTFLineInfo))]
+    public class MeasureCNTKTextFormat : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string Path;
+
+        protected override void BeginProcessing()
+        {
+            // Get absolute path
+
+            var current = SessionState.Path.CurrentFileSystemLocation;
+
+            if (!System.IO.Path.IsPathRooted(Path))
+                Path = SessionState.Path.Combine(current.ToString(), Path);
+
+            // Count sequeneces
+
+            Tuple<int, int> lines;
+            using (var reader = new StreamReader(Path, Encoding.UTF8))
+            {
+                lines = CTFTools.CountLines(reader);
+            }
+
+            WriteObject(new CTFLineInfo() { Lines = lines.Item1, Sequences = lines.Item2 });
+        }
+    }
+}
