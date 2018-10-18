8ae5962ac6ba11d260184eef657185a729ef6cf2
horker <horker@users.noreply.github.com>
2018-10-19 00:43:42 +0900

    Update example/mnist to fit the new training session interface

--- examples/mnist/Train-Mnist.ps1
+++ examples/mnist/Train-Mnist.ps1
@@ -1,5 +1,5 @@
 param(
-  [switch]$UseConv
+    [switch]$UseConv
 )
 
 Set-StrictMode -Version Latest
@@ -8,7 +8,7 @@ Set-StrictMode -Version Latest
 # Configuration
 ############################################################
 
-$MNIST_CACHE_FILE = "$PSScriptRoot\mnist_data.bin"
+$MNIST_DATA_FILE = "$PSScriptRoot\mnist_data.bin"
 
 $OUT_CLASSES = 10
 
@@ -18,10 +18,10 @@ Set-CNTKRandomSeed 1234
 # Data
 ############################################################
 
-$data = cntk.datasourceset -Path $MNIST_CACHE_FILE
+$data = cntk.datasourceset -Path $MNIST_DATA_FILE
 $trainData, $testData = $data.Split(@(.8, .2))
 $sampler = cntk.sampler $trainData -MinibatchSize 64
-$testSampler = cntk.sampler $testData -MinibatchSize 64
+$testSampler = cntk.sampler $testData -MinibatchSize 64 -NoRandomize
 
 ############################################################
 # Model
@@ -31,22 +31,26 @@ $in = cntk.input (28, 28, 1) -Name input
 $n = $in
 
 if ($UseConv) {
-  # conv1: 28 x 28 x 1 -> 28 x 28 x 8 -> 14 x 14 x 8
-  $n = cntk.conv2d $n (3, 3) 8 (1, 1) $true (cntk.init.truncatednormal .1) relu
-  $n = cntk.maxpooling $n (3, 3) (2, 2)
+    # conv1: 28 x 28 x 1 -> 28 x 28 x 8 -> 14 x 14 x 8
+    $n = cntk.conv2d $n (3, 3) 8 (1, 1) $true (cntk.init.truncatednormal .1)
+    $n = cntk.batchnorm $n
+    $n = cntk.relu $n
+    $n = cntk.maxpooling $n (3, 3) (2, 2)
 
-  # conv2: 14 x 14 x 8 -> 14 x 14 x 32 -> 7 x 7 x 32
-  $n = cntk.conv2d $n (3, 3) 32 (1, 1) $true (cntk.init.truncatednormal .1) relu
-  $n = cntk.maxpooling $n (3, 3) (2, 2)
+    # conv2: 14 x 14 x 8 -> 14 x 14 x 32 -> 7 x 7 x 32
+    $n = cntk.conv2d $n (3, 3) 32 (1, 1) $true (cntk.init.truncatednormal .1)
+    $n = cntk.batchnorm $n
+    $n = cntk.relu $n
+    $n = cntk.maxpooling $n (3, 3) (2, 2)
 
-  # fc
-  $n = cntk.dense $n $OUT_CLASSES (cntk.init.glorotuniform)
+    # fc
+    $n = cntk.dense $n $OUT_CLASSES (cntk.init.glorotuniform)
 }
 else {
-  $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
-  $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
-  $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
-  $n = cntk.dense $n $OUT_CLASSES (cntk.init.glorotuniform)
+    $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
+    $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
+    $n = cntk.dense $n 1000 (cntk.init.heuniform) relu
+    $n = cntk.dense $n $OUT_CLASSES (cntk.init.glorotuniform)
 }
 
 $out = $n
@@ -57,8 +61,13 @@ $label = cntk.input $OUT_CLASSES -Name label
 # Training
 ############################################################
 
-$learner = cntk.momentumsgd $out .1 .9
-
-$trainer = cntk.trainer $out $label CrossEntropyWithSoftmax ClassificationError $learner
-
-cntk.starttraining $trainer $sampler $testSampler -MaxIteration 10000 -ProgressOutputStep 500 -LogFile "$PSScriptRoot\mnist_log.log"
+cntk.starttraining `
+    -Model $out `
+    -LossFunction (cntk.crossEntropyWithSoftmax $out $label) `
+    -EvaluationFunction (cntk.classificationError $out $label) `
+    -Learner (cntk.adam $out .1) `
+    -Sampler $sampler `
+    -ValidationSampler $testSampler `
+    -MaxIteration 20000 `
+    -ProgressOutputStep 500 `
+    -LogFile "$PSScriptRoot\mnist_log.log"
