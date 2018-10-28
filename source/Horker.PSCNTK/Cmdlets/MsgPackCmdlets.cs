e15a693b43f48ebecd1ae242df539c8923da8e3c
horker <horker@users.noreply.github.com>
2018-10-28 22:12:22 +0900

    Rename Set/Get/Add-CNTKMsgPack to Export/Import-CNTKMsgPack

--- source/Horker.PSCNTK/Cmdlets/MsgPackCmdlets.cs
+++ source/Horker.PSCNTK/Cmdlets/MsgPackCmdlets.cs
@@ -6,8 +6,8 @@ using MsgPack.Serialization;
 
 namespace Horker.PSCNTK
 {
-    [Cmdlet("Set", "CNTKMsgPack")]
-    public class SetCNTKMsgPack : PSCmdlet
+    [Cmdlet("Export", "CNTKMsgPack")]
+    public class ExportCNTKMsgPack : PSCmdlet
     {
         [Parameter(Position = 0, Mandatory = true)]
         public DataSourceSet DataSourceSet;
@@ -21,11 +21,15 @@ namespace Horker.PSCNTK
         [Parameter(Position = 3, Mandatory = false)]
         public SwitchParameter OmitFraction = false;
 
+        [Parameter(Position = 4, Mandatory = false)]
+        public SwitchParameter Append = false;
+
         protected override void BeginProcessing()
         {
             var path = IO.GetAbsolutePath(this, Path);
+            var fileMode = Append ? FileMode.Append : FileMode.Create;
 
-            using (var stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            using (var stream = new FileStream(path, fileMode, FileAccess.Write))
             {
                 var count = DataSourceSet.SampleCount;
                 for (var i = 0; i < count; i += SplitSize)
@@ -41,43 +45,8 @@ namespace Horker.PSCNTK
         }
     }
 
-    [Cmdlet("Add", "CNTKMsgPack")]
-    public class AddCNTKMsgPack : PSCmdlet
-    {
-        [Parameter(Position = 0, Mandatory = true)]
-        public DataSourceSet DataSourceSet;
-
-        [Parameter(Position = 1, Mandatory = true)]
-        public string Path;
-
-        [Parameter(Position = 2, Mandatory = false)]
-        public int SplitSize = int.MaxValue;
-
-        [Parameter(Position = 3, Mandatory = false)]
-        public SwitchParameter OmitFraction = false;
-
-        protected override void BeginProcessing()
-        {
-            var path = IO.GetAbsolutePath(this, Path);
-
-            using (var stream = new FileStream(path, FileMode.Append, FileAccess.Write))
-            {
-                var count = DataSourceSet.SampleCount;
-                for (var i = 0; i < count; i += SplitSize)
-                {
-                    var size = Math.Min(SplitSize, count - i);
-                    if (OmitFraction && size < SplitSize)
-                        break;
-
-                    var chunk = DataSourceSet.Slice(i, size);
-                    MsgPackSerializer.Serialize(chunk, stream);
-                }
-            }
-        }
-    }
-
-    [Cmdlet("Get", "CNTKMsgPack")]
-    public class GetCNTKMsgPack : PSCmdlet
+    [Cmdlet("Import", "CNTKMsgPack")]
+    public class ImportCNTKMsgPack : PSCmdlet
     {
         [Parameter(Position = 0, Mandatory = true)]
         public string Path;
--- tests/DataSourceSet.Tests.ps1
+++ tests/DataSourceSet.Tests.ps1
@@ -1,15 +1,15 @@
-Describe "Set-CNTKMsgPack/Get-CNTKMsgPack" {
+Describe "Export/Import-CNTKMsgPack" {
 
   $file = "$PSScriptRoot\temp.msgpack"
 
-  It "can save and load a DataSourceSet in MsgPack" {
+  It "can export and import a DataSourceSet in MsgPack" {
       $a = cntk.dataSource (1, 2, 3, 4) (2, 2, 1)
       $b = cntk.dataSource (10, 20, 30, 40, 50, 60) (2, 3, 1)
       $dss = cntk.dataSourceSet @{ a = $a; b = $b }
 
-      Set-CNTKMsgPack $dss $file
+      Export-CNTKMsgPack $dss $file
 
-      $result = Get-CNTKMsgPack $file
+      $result = Import-CNTKMsgPack $file
 
       $result.Features.Count | Should -Be 2
 
@@ -19,16 +19,16 @@ Describe "Set-CNTKMsgPack/Get-CNTKMsgPack" {
       $d.Data | Should -Be (1, 2, 3, 4)
   }
 
-  It "can save and load multiple DataSourceSets in MsgPack" {
+  It "can append multiple DataSourceSets to the file" {
       $a = cntk.dataSource (1, 2, 3, 4) (2, 2, 1)
       $dss = cntk.dataSourceSet @{ a = $a }
-      Set-CNTKMsgPack $dss $file
+      Export-CNTKMsgPack $dss $file
 
       $b = cntk.dataSource (10, 20, 30, 40, 50, 60) (2, 3, 1)
       $dss = cntk.dataSourceSet @{ b = $b }
-      Add-CNTKMsgPack $dss $file
+      Export-CNTKMsgPack $dss $file -Append
 
-      $results = Get-CNTKMsgPack $file
+      $results = Import-CNTKMsgPack $file
 
       $results.Count | Should -Be 2
 
@@ -43,24 +43,24 @@ Describe "Set-CNTKMsgPack/Get-CNTKMsgPack" {
       $d.Data | Should -Be (10, 20, 30, 40, 50, 60)
   }
 
-  It "can save and load split DataSourceSets in MsgPack" {
-      $a = cntk.dataSource (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14) (2, 7)
+  It "can split DataSourceSets" {
+      $a = cntk.dataSource (1..14) (2, 7)
       $dss = cntk.dataSourceSet @{ a = $a }
-      Set-CNTKMsgPack $dss $file -SplitSize 3
+      Export-CNTKMsgPack $dss $file -SplitSize 3
 
-      $results = Get-CNTKMsgPack $file
+      $results = Import-CNTKMsgPack $file
 
       $results.Count | Should -Be 3
 
       $d = $results[0]["a"]
       $d | Should -BeOfType [Horker.PSCNTK.IDataSource[float]]
       $d.Shape.Dimensions | Should -Be (2, 3)
-      $d.Data | Should -Be (1, 2, 3, 4, 5, 6)
+      $d.Data | Should -Be (1..6)
 
       $d = $results[1]["a"]
       $d | Should -BeOfType [Horker.PSCNTK.IDataSource[float]]
       $d.Shape.Dimensions | Should -Be (2, 3)
-      $d.Data | Should -Be (7, 8, 9, 10, 11, 12)
+      $d.Data | Should -Be (7..12)
 
       $d = $results[2]["a"]
       $d | Should -BeOfType [Horker.PSCNTK.IDataSource[float]]
@@ -68,23 +68,23 @@ Describe "Set-CNTKMsgPack/Get-CNTKMsgPack" {
       $d.Data | Should -Be (13, 14)
   }
 
-  It "can omit fraction in MsgPack" {
-      $a = cntk.dataSource (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14) (2, 7)
+  It "can omit fraction" {
+      $a = cntk.dataSource (1..14) (2, 7)
       $dss = cntk.dataSourceSet @{ a = $a }
-      Set-CNTKMsgPack $dss $file -SplitSize 3 -OmitFraction
+      Export-CNTKMsgPack $dss $file -SplitSize 3 -OmitFraction
 
-      $results = Get-CNTKMsgPack $file
+      $results = Import-CNTKMsgPack $file
 
       $results.Count | Should -Be 2
 
       $d = $results[0]["a"]
       $d | Should -BeOfType [Horker.PSCNTK.IDataSource[float]]
       $d.Shape.Dimensions | Should -Be (2, 3)
-      $d.Data | Should -Be (1, 2, 3, 4, 5, 6)
+      $d.Data | Should -Be (1..6)
 
       $d = $results[1]["a"]
       $d | Should -BeOfType [Horker.PSCNTK.IDataSource[float]]
       $d.Shape.Dimensions | Should -Be (2, 3)
-      $d.Data | Should -Be (7, 8, 9, 10, 11, 12)
+      $d.Data | Should -Be (7..12)
   }
 }
