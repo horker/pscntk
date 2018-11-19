02c5d980d0f6e3e70101db84870690e4a517cc11
horker <horker@users.noreply.github.com>
2018-11-19 23:06:20 +0900

    Implement cntk.value

--- source/Horker.PSCNTK/Cmdlets/ValueCmdlets.cs
+++ source/Horker.PSCNTK/Cmdlets/ValueCmdlets.cs
@@ -3,15 +3,40 @@ using CNTK;
 
 namespace Horker.PSCNTK
 {
+    [Cmdlet("New", "CNTKValue")]
+    [Alias("cntk.value")]
+    [OutputType(typeof(Value))]
+    public class NewCNTKValue : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public float[] Values;
+
+        [Parameter(Position = 1, Mandatory = false)]
+        public int[] Dimensions = null;
+
+        [Parameter(Position = 2, Mandatory = false)]
+        public DeviceDescriptor Device = null;
+
+        protected override void EndProcessing()
+        {
+            if (Dimensions == null)
+                Dimensions = new int[] { Values.Length };
+
+            var value = ValueMethods.SafeCreate(Dimensions, Values, Device);
+            WriteObject(value);
+        }
+    }
+
     [Cmdlet("New", "CNTKBatchOfSequences")]
     [Alias("cntk.batchOfSequences")]
+    [OutputType(typeof(Value))]
     public class NewCNTKBatchOfSequenses : PSCmdlet
     {
         [Parameter(Position = 0, Mandatory = true)]
-        public int[] Dimensions;
+        public float[][] Values;
 
         [Parameter(Position = 1, Mandatory = true)]
-        public float[][] Values;
+        public int[] Dimensions;
 
         [Parameter(Position = 2, Mandatory = false)]
         public DeviceDescriptor Device = null;
