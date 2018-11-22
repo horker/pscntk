57da82444c583b1685ba10fb9a60d107f0056f37
horker <horker@users.noreply.github.com>
2018-11-22 20:25:50 +0900

    Add examples\mnist\Retrain.ps1

--- examples/mnist/Retrain.ps1
+++ examples/mnist/Retrain.ps1
@@ -1,7 +1,3 @@
-param(
-    [switch]$UseConv
-)
-
 Set-StrictMode -Version Latest
 
 ############################################################
@@ -12,7 +8,8 @@ $MNIST_DATA_FILE = "$PSScriptRoot\mnist_data.bin"
 
 $OUT_CLASSES = 10
 
-$MODEL_FILE = "$PSScriptRoot\mnist.model"
+$IN_MODEL_FILE = "$PSScriptRoot\mnist.model"
+$OUT_MODEL_FILE = "$PSScriptRoot\mnist_retrain.model"
 
 Set-CNTKRandomSeed 1234
 
@@ -29,31 +26,7 @@ $testSampler = cntk.sampler $testData -MinibatchSize 64 -NoRandomize
 # Model
 ############################################################
 
-$in = cntk.input (28, 28, 1) -Name input
-$n = $in
-
-if ($UseConv) {
-    # conv1: 28 x 28 x 1 -> 28 x 28 x 32 -> 14 x 14 x 32
-    $n = cntk.conv2d $n (5, 5) 32 (1, 1) $true (cntk.init.truncatednormal .1)
-    $n = cntk.batchnorm $n
-    $n = cntk.relu $n
-    $n = cntk.maxpooling $n (3, 3) (2, 2)
-
-    # conv2: 14 x 14 x 32 -> 14 x 14 x 64 -> 7 x 7 x 64
-    $n = cntk.conv2d $n (5, 5) 64 (1, 1) $true (cntk.init.truncatednormal .1)
-    $n = cntk.batchnorm $n
-    $n = cntk.relu $n
-    $n = cntk.maxpooling $n (3, 3) (2, 2)
-
-    # fc
-    $n = cntk.dense $n $OUT_CLASSES
-}
-else {
-    $n = cntk.dense $n 64 (cntk.init.heuniform) relu
-    $n = cntk.dense $n $OUT_CLASSES
-}
-
-$out = $n
+$out = Import-CNTKFunction $IN_MODEL_FILE
 
 $label = cntk.input $OUT_CLASSES -Name label
 
@@ -68,8 +41,8 @@ cntk.starttraining `
     -Learner (cntk.adam $out .1) `
     -Sampler $sampler `
     -ValidationSampler $testSampler `
-    -MaxIteration 20000 `
+    -MaxIteration 10000 `
     -ProgressOutputStep 500 `
-    -LogFile "$PSScriptRoot\mnist_log.log"
+    -LogFile "$PSScriptRoot\mnist_retrain.log"
 
-$out.Save($MODEL_FILE)
+$out.Save($OUT_MODEL_FILE)
